299bc79430a3c1d2565102860b22607d9bbe6695
DevLatBo <oskr.cbba2010@gmail.com>
2025-06-14 19:30:47 -0400

    [M] First modification

--- Model/ResourceModel/Post.php
+++ Model/ResourceModel/Post.php
@@ -9,7 +9,7 @@ class Post extends AbstractDb
 {
 
     /** @var string Main Table Name */
-    const MAIN_TABLE = 'blog_post';
+    const MAIN_TABLE = 'devlat_blog_post';
     /** @var string Main table primary key field name */
     const ID_FIELD_NAME = 'id';
 
